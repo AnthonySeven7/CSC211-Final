f060496a51eda2c99041fab9a4a7c1a9b07c5f14
Najib_uri_211 <najib_ishaq@my.uri.edu>
2018-04-04 09:02:14 -0400

    Made initial setup for starter code. Will finish soon.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * Started by Najib 3/21/18
+ **/
+
+#include "functions/functions.h"
+#include "functions/bigint.h"
+
+
+int main(int argc, char *argv[]) {
+    ///////////////////////////////////////////////////////////////////
+    // MAKE YOUR OWN MAIN.
+    ///////////////////////////////////////////////////////////////////
+
+    (void) argc;
+    (void) argv;
+
+    return 0;
+}
