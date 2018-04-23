94171794ad4c65f5113c042b06e26e66a024c52a
Greatgamer7 <anthony.neves77@gmail.com>
2018-04-23 17:00:43 -0400

    Finishing the Project

--- functions.cpp
+++ functions.cpp
@@ -8,56 +8,41 @@
 ///////////////////////////////////////////////////////////////////
 #include "functions.h"
 #include "bigint/bigint.h"
-#include <vector>
-#include <string>
+#include <stdlib.h>
+#include <math.h>
+
 
 
 ///////////////////////////////////////////////////////////////////
 // FUNCTION DEFINITIONS GO HERE
 ///////////////////////////////////////////////////////////////////
 
-std::vector<std::string> trigrams(const std::string language){ // Calculate all of the trigrams in the selected language
+int freqCalc(std::string trigram) { // Calculate all the frequencies
+  // Input: A map of all the trigrams Ex: ["abc","bcd","cde",...]
+  // Output: A vetor of the all the calculated frequencies in the language Ex: [0,1,2,...]
+  int frequency = 0; // An integer to get the summation of each trigrams letter
+  int num = 2;
+  for (int n = 0; n < 3; n ++) { // Iterate through the entire trigram
+    frequency += (((int)(trigram[n]%97))*(pow(26,num))); //Add calculated frequency to the variable frequency
+    num --;
+  }
+  return frequency;
+}
+
+std::vector<int> trigrams(const std::string language){ // Calculate all of the trigrams in the selected language
   // Input: The selected language
   // Output: An array of all the trigrams in the selected language Ex: ['abc','bcd','cde',...]
   // Initialize Variables
-  std::vector<std::string> tri_map; // Vector to hold all trigrams calculated
+  std::vector<int> map(17575,0); // Initialize empty vector map of each possible frequency of trigram
   std::string trigram = ""; // String to hold each trigram
-  for (int i = 0; i < language.length()-3; i++) { // Iterate through each letter in the selected language minus the final two as that will not be a trigram
+  int freq;
+  for (int i = 0; i < ((int)language.size())-2; i++) { // Iterate through each letter in the selected language minus the final two as that will not be a trigram
     trigram += language[i]; // Add the current letter to trigram
     trigram += language[i+1]; // Add the next letter to trigram
     trigram += language[i+2]; // Add the final letter to the trigram
-    tri_map.pushback(trigram); // Add trigram to trigram map
+    freq = freqCalc(trigram);
+    map[freq] += 1;
     trigram = ""; // Reset trigram
-  }
-  return tri_map; // Return the map of all trigrams
-}
-
-std::vector<int> freqCalc(const std::vector<std::string> tri_map) { // Calculate all the frequencies
-  // Input: A map of all the trigrams Ex: ["abc","bcd","cde",...]
-  // Output: A vetor of the all the calculated frequencies in the language Ex: [0,1,2,...]
-  std::vector<int> freq_map; // An empty vector that will hold all trigram frequencies for the given language
-  int frequency = 0; // An integer to get the summation of each trigrams letter
-  int letter_freq; // An integer to represent the frequency the individual trigrams
-  for (int i= 0; i < tri_map.size(); i ++) { // Iterate through the entire trigram map
-    trigram = tri_map[i]; // Call the current trigram
-    for (int n = 2; n >= 0; n --) { // Iterate through the entire trigram
-      letter_freq = (atoi(trigram[n])); // Convert the letter in the trigram into its ascii value
-      if (letter_freq < 91) letter_freq += 32; // If current letter is capital, convert the ascii of the capital letter t the ascii of its lowercase equivalent
-      letter_freq = (letter_freq % 97); // Convert the ascii value of the letter to its position in the alphabet
-      frequency += (letter_freq*(26^(n))); //Add calculated frequency to the variable frequency
-    }
-    freq_map.push_back(frequency); // Add the frequency to the frequency map
-    frequency = 0; // Reset the variable frequency
-  }
-  return freq_map; // Return the frequency map
-}
-
-std::vector<int> freqMap(const std::string language) {
-  std::vector<int> map(308915776); // Initialize empty vector map of each possible frequency of trigram
-  tri_map = trigrams(language); // Calculate trigrams
-  freq_map = freqCalc(tri_map); // Calculate frequencies
-  for (int i = 0; i < freq_map.size(); i ++) { // Iterate through each frequency that was calculated
-    map[freq_map[i]] += 1; // Go through the map and add one to any place where the frequency exists on the frequency map
   }
   return map; // Return the map with all of the frequencies
 }
--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@
 #include "bigint/bigint.h"
 #include <string>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <fstream>
 
 
 int main(int argc, char *argv[]) {
@@ -20,31 +18,35 @@ int main(int argc, char *argv[]) {
   }
   else {
     std::ifstream infile; // Initialize file to be opened
-    std::string filename = "testing_languages/" + argv[0]; // Get the file in the testing_languages folder
+    std::string filename = (((std::string)"testing_languages/") + argv[1]); // Get the file in the testing_languages folder
+    std::string sentence;
     infile.open(filename); // Open the file
-    std::vector<std::string>::iterator found;
-    std::vector<std::string> excempt = [" ","!","?",".",","] // A list of charcters to excempt from the final string
     if (!infile.fail()){
       char ch;
       while (infile.get(ch)){
-        found = (std::find(excempt.begin(),excempt.end(),char) != excempt.end()); // Check if the current character is in the character excempt list
-        if (found == excempt.end()) { // If current character is not in the excempt vector...
-          sentence += ch; // Add the character to the string
+        if ((int)ch >= 97 && (int)ch <= 122) {
+        sentence += ch; // Add the character to the string
+        }
+        else if ((int)ch >= 65 && (int)ch <= 90) {
+          ch += 32;
+          sentence += ch;
         }
       }
       infile.close(); // Close the file
-      std::vector<int> map = freqMap(sentence); // Start the calculations of the language
-      for (int i = 0; i < map.size(); i++) { // Iterate through the frequency map
-          if (i == map.size()-1) { // If the frequency of the very last trigram...
-            std::cout << map[i] << "\n"; // Display it with a new-line character
-          }
-          else {
-            std::cout << map[i] << " "; // Display the current frequency separating each by a space
-          }
       }
-    }
     else { // If the file cannot be opened...
       std::cerr << "Could not open file: " << filename << std::endl; // Display error message
       exit(EXIT_FAILURE);
     }
+    std::vector<int> map = trigrams(sentence); // Start the calculations of the language
+      for (int i = 0; i < ((int)map.size()); i++) { // Iterate through the frequency map
+          if (i == ((int)map.size())-1) { // If the frequency of the very last trigram...
+            std::cout << map[i] << std::endl; // Display it with a new-line character
+          }
+          else {
+              std::cout << map[i] << " "; // Display the current frequency separating each by a space
+          }
+      }
   }
+  return 0;
+}
